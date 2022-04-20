bfb1a5738877c2ac378c5d56febd9b7d740e3a64
Sergey <grasielo@inbox.ru>
2022-04-20 14:04:54 +0300

    positions and webp

--- components/NonParallaxSection.vue
+++ components/NonParallaxSection.vue
@@ -5,7 +5,7 @@
       <img
         :class="{'h-112 object-cover object-center w-full':$device.isTablet, 'h-104 object-cover object-center' : $device.isMobile}"
         width="390" height="416"
-        :src="SectionData.bgImage" alt=""/>
+        :src="$device.isDesktopOrTablet ? SectionData.bgImage : SectionData.bgImageMobile" alt=""/>
     </div>
     <div v-if="SectionData.title || SectionData.subtitle"
          class="text-white flex flex-col justify-center items-center font-black"
