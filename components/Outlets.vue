c9746e19a31d368bd9eb3bbce6036355891f4b32
Your Name <you@example.com>
2022-05-03 16:55:32 +0300

    outlets

--- components/Outlets.vue
+++ components/Outlets.vue
@@ -5,7 +5,7 @@
         <div class="flex flex-col justify-start items-center">
           <img
             class="border-2 border-black bg-light-yellow-100"
-            :class="{'h-136': $device.isDesktop, 'h-104' :$device.isTablet && $mq === 'lg', 'h-80' : $device.isTablet && $mq !== 'lg' }"
+            :class="{'h-136': $device.isDesktop, 'h-104' :$device.isTablet && $mq === 'lg', 'h-80' : $device.isTablet && $mq !== 'lg', 'w-11/12': $device.isMobile}"
             :src="item['img']"
             :srcset="`${item['img-2x']} 2x, ${item['img-3x']} 3x`"
             :width="$device.isDesktop ? 544 : ($device.isMobile ? 544 : ($device.isTablet ? 320 : ''))"
@@ -14,7 +14,7 @@
           <div
             class="flex flex-col items-center justify-center py-4 h-28 text-center bg-white border-black border-l-2 border-r-2 border-b-2"
             :class="{'h-36' : $device.isTablet && $mq !== 'lg', 'w-136':$device.isDesktop, 'w-104' :$device.isTablet && $mq === 'lg', 'w-80' : $device.isTablet && $mq !== 'lg',
-             'w-full' : $device.isMobile}">
+             'w-11/12' : $device.isMobile}">
             <span class="font-black leading-6"
                   :class="{'text-xl' : $device.isDesktop || $device.isMobile, 'text-base' :$device.isTablet && $mq !== 'lg'}"
                   v-html="item.description"></span>
