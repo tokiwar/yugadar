77198b5c31912a688df5fa5e951d335e93472fad
Sergey <grasielo@inbox.ru>
2022-05-12 15:57:53 +0300

    seo for mobile

--- components/catalog/CatalogItemMobile.vue
+++ components/catalog/CatalogItemMobile.vue
@@ -1,9 +1,17 @@
 <template>
   <div itemtype="https://schema.org/Product" itemscope class="flex flex-col justify-start items-center space-y-2">
-    <img :src="item.image" :alt="item.name" :title="item.name" width="356" height="356"/>
-    <span class="text-xl font-black">{{ item.name }}</span>
+    <img itemprop="image" :src="item.image" :alt="item.name" :title="item.name" width="356" height="356"/>
+    <meta itemprop="brand" content="ЮгаДар"/>
+    <span itemprop="name" class="text-xl font-black" v-html="item.name"/>
     <span class="h-0.5 w-2/6 bg-gray-500 my-2 mx-auto"></span>
-    <span class="ml-4 pb-2 text-lg italic">{{ item.description }}</span>
+    <span itemprop="description" class="ml-4 pb-2 text-lg italic" v-html="item.description"/>
+    <div itemprop="offers" itemtype="https://schema.org/Offer" itemscope>
+      <meta itemprop="availability" content="https://schema.org/InStock"/>
+      <meta itemprop="priceCurrency" content="RUB"/>
+      <meta itemprop="price" :content="parseInt(item.prices[0], 10)"/>
+      <meta itemprop="minPrice" :content="parseInt(item.prices[0], 10)"/>
+      <meta itemprop="maxPrice" :content="parseInt(item.prices[1], 10)"/>
+    </div>
   </div>
 </template>
 <script>
