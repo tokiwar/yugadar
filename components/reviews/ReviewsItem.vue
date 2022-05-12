42a0f66a5532de4672370804e572ed873764c9de
Sergey <grasielo@inbox.ru>
2022-05-12 09:56:32 +0300

    reviewedItem

--- components/reviews/ReviewsItem.vue
+++ components/reviews/ReviewsItem.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="flex flex-col justify-center items-center h-auto">
     <div
-      itemprop="review" itemscope="" itemtype="http://schema.org/Review"
       class="flex flex-col items-center justify-start text-justify border-2 bg-white border-black shadow-lg"
       :class="{'w-152 space-y-8 h-96 py-8' : $device.isDesktop, 'w-11/12 space-y-2 h-72 py-4' :$device.isMobile}">
       <div class="flex flex-col w-5/6">
-              <span itemprop="author" itemscope itemtype="https://schema.org/Person" :class="{'text-3xl':$device.isDesktopOrTablet, 'text-xl' : $device.isMobile}">
-                <span itemprop="name" v-html="item.name"/>
+              <span class="{'text-3xl':$device.isDesktopOrTablet, 'text-xl' : $device.isMobile}">
+                <span v-html="item.name"/>
               </span>
         <span class="w-5/6 underline"
-              itemprop="datePublished" :content="item.date"
               :class="{'text-base' : $device.isDesktopOrTablet, 'text-sm' : $device.isMobile}"
               v-html="item.date"></span>
       </div>
-      <span itemprop="reviewBody" class="w-5/6 text-between"
+      <span class="w-5/6 text-between"
             :class="{'text-lg' : $device.isDesktop, 'text-sm' : $device.isMobile }"
             v-html="item.text"></span>
     </div>
@@ -22,6 +20,37 @@
 <script>
 export default {
   name: 'ReviewsItem',
-  props: ['item']
+  props: ['item'],
+  head() {
+    return {
+      script: [
+        {
+          type: 'application/ld+json',
+          json: {
+            "@context": "https://schema.org/",
+            "@type": "Review",
+            "author": {
+              "@type": "Person",
+              "name": this.item.name
+            },
+            "itemReviewed": {
+              "@type": "Organization",
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Тимашевск",
+                "postalCode": "350056",
+                "streetAddress": "пос. Индустриальный, Центральная усадьба"
+              },
+              "email": "mamay4339061@gmail.com",
+              "name": "ЮгаДар",
+              "telephone": "+79184339061"
+            },
+            "datePublished": this.item.date,
+            "reviewBody": this.item.text,
+          }
+        }
+      ]
+    }
+  }
 }
 </script>
