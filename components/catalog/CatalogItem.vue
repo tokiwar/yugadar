c7d061d2dfa89a915b801e0f738b72ce6385e50a
Sergey <grasielo@inbox.ru>
2022-05-12 15:31:51 +0300

    catalog seo

--- components/catalog/CatalogItem.vue
+++ components/catalog/CatalogItem.vue
@@ -1,15 +1,17 @@
 <template>
   <div itemtype="https://schema.org/Product" itemscope class="flex flex-col items-center">
-    <img :src="item.image" width="232" height="232" :alt="item.name" :title="item.name"/>
+    <img itemprop="image" :src="item.image" width="232" height="232" :alt="item.name" :title="item.name"/>
     <div class="flex flex-col pt-4 h-48">
       <span itemprop="name" class="text-2xl h-16 flex items-center justify-center" v-html="item.nameHtml"></span>
-      <span itemprop="image" class="h-0.5 w-64 bg-gray-500 my-2 mx-auto"></span>
+      <span class="h-0.5 w-64 bg-gray-500 my-2 mx-auto"/>
       <span itemprop="description" class="italic h-20" v-html="item.description "/>
       <div itemprop="offers" itemtype="https://schema.org/Offer" itemscope>
-        <meta itemprop="availability" content="https://schema.org/InStock" />
+        <meta itemprop="brand" content="ЮгаДар"/>
+        <meta itemprop="availability" content="https://schema.org/InStock"/>
         <meta itemprop="priceCurrency" content="RUB"/>
-        <meta itemprop="minPrice" :content="parseInt(item.prices[0], 10)" />
-        <meta itemprop="maxPrice" :content="parseInt(item.prices[1], 10)" />
+        <meta itemprop="price" :content="parseInt(item.prices[0], 10)"/>
+        <meta itemprop="minPrice" :content="parseInt(item.prices[0], 10)"/>
+        <meta itemprop="maxPrice" :content="parseInt(item.prices[1], 10)"/>
       </div>
     </div>
   </div>
