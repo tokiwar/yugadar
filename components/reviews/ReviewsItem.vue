de327a98720827c49882e0e511453ef8ac70cdc2
Sergey <grasielo@inbox.ru>
2022-05-12 09:42:59 +0300

    schema fix

--- components/reviews/ReviewsItem.vue
+++ components/reviews/ReviewsItem.vue
@@ -5,8 +5,9 @@
       class="flex flex-col items-center justify-start text-justify border-2 bg-white border-black shadow-lg"
       :class="{'w-152 space-y-8 h-96 py-8' : $device.isDesktop, 'w-11/12 space-y-2 h-72 py-4' :$device.isMobile}">
       <div class="flex flex-col w-5/6">
-              <span itemprop="author" :class="{'text-3xl':$device.isDesktopOrTablet, 'text-xl' : $device.isMobile}"
-                    v-html="item.name"></span>
+              <span itemprop="author" itemscope itemtype="https://schema.org/Person" :class="{'text-3xl':$device.isDesktopOrTablet, 'text-xl' : $device.isMobile}">
+                <span itemprop="name" v-html="item.name"/>
+              </span>
         <span class="w-5/6 underline"
               itemprop="datePublished" :content="item.date"
               :class="{'text-base' : $device.isDesktopOrTablet, 'text-sm' : $device.isMobile}"
