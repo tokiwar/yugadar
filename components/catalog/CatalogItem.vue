2852b9b2112ef4b8261497a9e1d4395eed5c64a3
Sergey <grasielo@inbox.ru>
2022-05-12 15:41:52 +0300

    brand

--- components/catalog/CatalogItem.vue
+++ components/catalog/CatalogItem.vue
@@ -1,12 +1,12 @@
 <template>
   <div itemtype="https://schema.org/Product" itemscope class="flex flex-col items-center">
     <img itemprop="image" :src="item.image" width="232" height="232" :alt="item.name" :title="item.name"/>
+    <meta itemprop="brand" content="ЮгаДар"/>
     <div class="flex flex-col pt-4 h-48">
       <span itemprop="name" class="text-2xl h-16 flex items-center justify-center" v-html="item.nameHtml"></span>
       <span class="h-0.5 w-64 bg-gray-500 my-2 mx-auto"/>
       <span itemprop="description" class="italic h-20" v-html="item.description "/>
       <div itemprop="offers" itemtype="https://schema.org/Offer" itemscope>
-        <meta itemprop="brand" content="ЮгаДар"/>
         <meta itemprop="availability" content="https://schema.org/InStock"/>
         <meta itemprop="priceCurrency" content="RUB"/>
         <meta itemprop="price" :content="parseInt(item.prices[0], 10)"/>
