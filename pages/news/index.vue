6d58dd07056a72350aabc3a275daa94f4fc02c05
Sergey <grasielo@inbox.ru>
2022-05-19 16:14:06 +0300

    new pages

---
+++ pages/news/index.vue
@@ -0,0 +1,56 @@
+<template>
+  Новости
+</template>
+<script>
+export default {
+  name: 'NewsPage',
+  head() {
+    return {
+      title: '«ЮгаДар» - Карта сайта - Новости и акции',
+      meta: this.meta,
+      link: [
+        {rel: 'canonical', href: 'https://yugadar.ru/news/'},
+      ]
+    }
+  },
+  async asyncData({route, $axios}) {
+    const meta = [
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: '«ЮгаДар» - Карта сайта - Новости и акции'
+      },
+      {
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://yugadar.ru/news/'
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Мороженое от кубанского производителя «ЮгаДар». Важные новости и события «ЮгаДар».'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Мороженое от кубанского производителя «ЮгаДар». Важные новости и события «ЮгаДар».'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'мороженое, сладкое, югадар, тимашевск, краснодарский край, новости, события, акции'
+      }
+    ];
+    if (Object.keys(route.query).length) {
+      meta.push(
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: 'noindex, nofollow'
+        }
+      );
+    }
+    return {meta}
+  },
+}
+</script>
---
+++ pages/site-map/index.vue
@@ -0,0 +1,56 @@
+<template>
+  Карта сайта
+</template>
+<script>
+export default {
+  name: 'SiteMapPage',
+  head() {
+    return {
+      title: '«ЮгаДар» - Карта сайта - Список страниц на сайте',
+      meta: this.meta,
+      link: [
+        {rel: 'canonical', href: 'https://yugadar.ru/site-map/'},
+      ]
+    }
+  },
+  async asyncData({route, $axios}) {
+    const meta = [
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: '«ЮгаДар» - Карта сайта - Список страниц на сайте'
+      },
+      {
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://yugadar.ru/site-map/'
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Мороженое от кубанского производителя «ЮгаДар». Список всех страниц на сайте.'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Мороженое от кубанского производителя «ЮгаДар». Список всех страниц на сайте.'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'мороженое, сладкое, югадар, тимашевск, краснодарский край, карта сайта'
+      }
+    ];
+    if (Object.keys(route.query).length) {
+      meta.push(
+        {
+          hid: 'robots',
+          name: 'robots',
+          content: 'noindex, nofollow'
+        }
+      );
+    }
+    return {meta}
+  },
+}
+</script>
