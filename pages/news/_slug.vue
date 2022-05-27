33365febc5204a6e6048ff758260fe5fb3772bec
Sergey <grasielo@inbox.ru>
2022-05-27 18:04:21 +0300

    news detail

--- pages/news/_slug.vue
+++ pages/news/_slug.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    {{ data }}
-  </div>
+  <section>
+    <top-text-section :section-data="logoData"/>
+    <div class="h-96 bg-pattern-lines">
+      {{ logoData }}
+    </div>
+  </section>
 </template>
 <script>
 export default {
-  async asyncData({ params, route, $axios }) {
+  data: () => ({
+    logoData: {
+      title: 'Открытие сайта',
+      bgColor: 'bg-light-pink-100',
+      text: 'Текст статьи',
+      image: 'Изображение статьи',
+      date: 'Дата'
+    }
+  }),
+  async asyncData({params, route, $axios}) {
     const data = params // When calling /abc the slug will be "abc"
-    return { data }
+    return {data}
   }
 }
 </script>
