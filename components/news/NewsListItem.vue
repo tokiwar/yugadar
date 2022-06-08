943095258f878273505eef324bd6969e7c0d8872
Sergey <grasielo@inbox.ru>
2022-06-08 10:09:53 +0300

    better seo author

--- components/news/NewsListItem.vue
+++ components/news/NewsListItem.vue
@@ -4,7 +4,9 @@
                class="flex flex-col justify-center items-center h-auto cursor-pointer hover:shadow-2xl">
       <meta itemprop="datePublished" :datetime="item.dateFormatted" :content="item.dateFormatted">
       <meta itemprop="description" :content="item.text">
-      <meta itemprop="author" content="ЮгаДар">
+      <div itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+        <meta itemprop="name" content="ЮгаДар">
+      </div>
       <div class="flex flex-col items-center justify-end text-justify glassmorphism relative"
            :class="{'w-152 space-y-8 h-88 py-6' : $device.isDesktopOrTablet, 'w-full space-y-2 h-48' :$device.isMobile}">
         <img
@@ -14,7 +16,8 @@
           :alt="item.name"/>
         <div class="z-10 text-white flex flex-col w-5/6" :class="{'absolute pb-2' : $device.isMobile}">
           <span class="font-bold text-base underline" v-html="item.date"/>
-          <span itemprop="headline name" class="font-black" :class="{'text-4xl' : $device.isDesktopOrTablet, 'text-2xl' : $device.isMobile}"
+          <span itemprop="headline name" class="font-black"
+                :class="{'text-4xl' : $device.isDesktopOrTablet, 'text-2xl' : $device.isMobile}"
                 v-html="item.name"/>
         </div>
       </div>
