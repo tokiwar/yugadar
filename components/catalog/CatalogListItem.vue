ac676107e392c9199d901b70dc1656c069a75764
Sergey <grasielo@inbox.ru>
2022-07-13 08:44:58 +0300

    prices

--- components/catalog/CatalogListItem.vue
+++ components/catalog/CatalogListItem.vue
@@ -1,13 +1,14 @@
 <template>
   <div
-    class="flex flex-row justify-between h-24 border-b-4 border-dashed text-gray-800">
+    class="flex flex-row justify-between h-24 border-b-4 border-dashed text-gray-800 w-full">
     <div itemprop="name"
          class="p-2 flex items-center justify-center"
-         :class="{'text-2xl' : $device.isDesktopOrTablet, 'text-sm w-3/6' : $device.isMobile}"
+         :class="{'text-2xl' : $device.isDesktopOrTablet, 'text-sm w-2/5' : $device.isMobile}"
          v-html="item.name"/>
-    <div class="flex flex-row items-center justify-center">
+    <div class="flex flex-row items-center justify-center"
+         :class="{'w-3/5' : $device.isMobile}">
       <div class="p-2 border-l-4 border-dashed items-center justify-center"
-           :class="{'text-xl' : $device.isDesktopOrTablet,'text-base w-3/6': $device.isMobile}"
+           :class="{'text-xl' : $device.isDesktopOrTablet,'text-xs w-full whitespace-nowrap': $device.isMobile}"
            v-for="price in item['prices-formatted']" v-html="price"/>
     </div>
   </div>
