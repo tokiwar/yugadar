f9880ff9e9ecc6bc2a25d69d35b68dc4f8725c48
Sergey <grasielo@inbox.ru>
2022-09-12 16:19:47 +0300

    better icon

--- components/ErrorPage.vue
+++ components/ErrorPage.vue
@@ -3,7 +3,7 @@
     <section class="bg-pattern-lines select-none py-10">
       <div class="font-black text-black flex flex-col justify-items items-center text-center"
            :class="{'space-y-6' : $device.isMobile}">
-        <svg-icon :class="{'h-120 w-120' : $device.isDesktop, 'w-88 h-88' : $device.isTablet, 'w-64 h-64' : $device.isMobile}" name="ice-fall"/>
+        <svg-icon :class="{'h-120 w-120' : $device.isDesktop, 'w-88 h-88' : $device.isTablet, 'w-64 h-64' : $device.isMobile}" name="ice-fall-long"/>
         <h1 :class="{'text-8xl' :$device.isDesktopOrTablet, 'text-5xl' : $device.isMobile}">Страница не найдена</h1>
         <NuxtLink
           :class="{'py-10 text-5xl': $device.isDesktopOrTablet, 'text-4xl underline text-yellow-500': $device.isMobile}"
