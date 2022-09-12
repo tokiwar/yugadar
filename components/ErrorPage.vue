10dddbfbf821e6ca9693c613df8d64f6ed42e73e
Sergey <grasielo@inbox.ru>
2022-09-12 16:10:02 +0300

    better 404 page

---
+++ components/ErrorPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <section class="bg-pattern-lines select-none py-10">
+      <div class="font-black text-black flex flex-col justify-items items-center text-center"
+           :class="{'space-y-6' : $device.isMobile}">
+        <svg-icon :class="{'h-120 w-120' : $device.isDesktop, 'w-88 h-88' : $device.isTablet, 'w-64 h-64' : $device.isMobile}" name="ice-fall"/>
+        <h1 :class="{'text-8xl' :$device.isDesktopOrTablet, 'text-5xl' : $device.isMobile}">Страница не найдена</h1>
+        <NuxtLink
+          :class="{'py-10 text-5xl': $device.isDesktopOrTablet, 'text-4xl underline text-yellow-500': $device.isMobile}"
+          class="inline-block hover:text-yellow-500 hover:underline" to="/">на главную
+        </NuxtLink>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ErrorPage'
+}
+</script>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-    <section class="bg-pattern-lines select-none py-40">
-      <div class="font-black text-gray-800 flex flex-col justify-items items-center text-center"
-           :class="{'space-y-6' : $device.isMobile}">
-        <h1 :class="{'text-15xl flex items-center justify-center h-80': $device.isDesktopOrTablet, 'text-9xl contents' :$device.isMobile}">404</h1>
-        <h2 :class="{'text-8xl' :$device.isDesktopOrTablet, 'text-5xl' : $device.isMobile}">Страница не найдена</h2>
-        <NuxtLink :class="{'py-10 text-5xl': $device.isDesktopOrTablet, 'text-4xl underline text-yellow-500': $device.isMobile}"
-                  class="inline-block hover:text-yellow-500 hover:underline" to="/">на главную
-        </NuxtLink>
-      </div>
-    </section>
+    <error-page/>
   </div>
 </template>
 
