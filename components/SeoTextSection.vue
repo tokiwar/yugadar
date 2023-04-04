219213a8b5fbbec0836b2dc08d357ebe351656ef
Your Name <you@example.com>
2023-04-04 21:03:38 +0300

    smaller text

--- components/SeoTextSection.vue
+++ components/SeoTextSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-10 select-none bg-pattern-lines flex flex-col justify-center items-center" v-if="html">
     <div class="w-3/5 flex flex-col space-y-6"
-         :class="{'w-3/5' : $device.isDesktopOrTablet, 'w-5/6' : !$device.isDesktopOrTablet}" v-html="html"/>
+         :class="{'w-3/5' : $device.isDesktopOrTablet, 'w-5/6 text-base' : !$device.isDesktopOrTablet}" v-html="html"/>
   </section>
 </template>
 <script>
