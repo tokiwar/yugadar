b2f714dc0c8ca22d3fd664d30182b4dd14e518af
Your Name <you@example.com>
2025-04-06 11:36:05 +0300

    better

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -5,14 +5,14 @@
     <bottom-menu :menu="menu" :class="{'w-auto' : $device.isDesktopOrTablet}"/>
     <div
       :class="{'flex flex-row w-auto items-start space-x-4' : $device.isDesktopOrTablet, 'flex flex-col justify-center items-center' : $device.isMobile}">
-      <span class="text-lg">©<span itemprop="copyrightYear">2023</span> «ЮгаДар»</span>
+      <span class="text-lg">©<span itemprop="copyrightYear">2025</span> «ЮгаДар»</span>
       <span class="flex"
             :class="{'flex-col items-center' : $device.isMobile, 'flex-row space-x-4 self-center' : $device.isDesktopOrTablet}">
         <NuxtLink class="text-sm text-gray-400 opacity-70"
                   to="/privacy-policy/">Политика конфиденциальности</NuxtLink>
           <NuxtLink class="text-sm text-gray-400 opacity-70"
                     to="/site-map/">Карта сайта</NuxtLink>
-        <a href="https://yugadar-it.ru" class="text-sm text-gray-400 opacity-70">Разработка - Yugadar.IT</a>
+        <a v-if="false" href="https://yugadar-it.ru" class="text-sm text-gray-400 opacity-70">Разработка - Yugadar.IT</a>
       </span>
     </div>
   </footer>
